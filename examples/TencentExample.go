216c04f1c69d32d2bc6c70aa29bb48eedd1ee8da
victorkowalski <vkowalski@yandex.ru>
2024-09-23 12:53:22 +0300

    argument example

--- examples/TencentExample.go
+++ examples/TencentExample.go
@@ -1,1 +1,24 @@
-package examples
+package main
+
+import (
+	"fmt"
+	api2captcha "github.com/2captcha/2captcha-go"
+	"os"
+)
+
+func main() {
+	//"06c869c488704f62826181f2562ac999"
+	//argsWithProg := os.Args
+	client := api2captcha.NewClient(os.Args[1])
+
+	captcha := api2captcha.MTCaptcha{
+		SiteKey: "MTPublic-KzqLY1cKH",
+		Url:     "https://2captcha.com/demo/mtcaptcha",
+	}
+	req := captcha.ToRequest()
+	code, code1, err := client.Solve(req)
+
+	fmt.Println(code)
+	fmt.Println(code1)
+	fmt.Println(err)
+}
