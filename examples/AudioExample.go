8aaf9c1401dbad3e743afeaba34b000dcff43048
victorkowalski <vkowalski@yandex.ru>
2024-10-03 11:28:03 +0300

    audio example

---
+++ examples/AudioExample.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	//client := api2captcha.NewClient(os.Args[1])
+
+	currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	assetsDir := currentDir + "/assets"
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(assetsDir)
+
+	//var buf bytes.Buffer
+
+	fileName := assetsDir + "/" + "audio-en.mp3"
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Read the file into a byte slice
+	bs := make([]byte, stat.Size())
+	_, err = bufio.NewReader(file).Read(bs)
+	if err != nil && err != io.EOF {
+		fmt.Println(err)
+		return
+	}
+
+	//data := buf.Bytes()
+	fileBase64Str := base64.StdEncoding.EncodeToString(bs)
+
+	fmt.Println(fileBase64Str)
+	/*
+		tencentCaptcha := api2captcha.Tencent{
+			AppId: "2092215077",
+			Url:   "http://lcec.lclog.cn/cargo/NewCargotracking?blno=BANR01XMHB0004&selectstate=BLNO",
+		}
+
+		req := tencentCaptcha.ToRequest()
+
+		token, captchaId, err := client.Solve(req)
+
+		fmt.Println("token ::: " + token)
+		fmt.Println("captchaId ::: " + captchaId)
+		fmt.Print("error ::: ")
+		fmt.Println(err)*/
+}
+
+/*
+dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(dir)
+*/
